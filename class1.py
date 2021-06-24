e4accbfa5232d4726c18de9cae32724c94de741a
harshachekuri <85867243+harshachekuri@users.noreply.github.com>
2021-06-24 13:22:58 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,11 @@
+class kkrksr:
+    def even(eve):
+        if eve%2==0:
+            return True
+        else:
+            return False
+    def odd(eve):
+        if eve%2==1:#!=0
+            return True
+        else:
+            return False
